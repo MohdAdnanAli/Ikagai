5ff7850e0468c635d32680f2df42b9b83d8adc5a
SirOmni <kartikeyawasthi1@gmail.com>
2023-09-06 21:10:40 +0530

    Dark mode

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="field input">
-          <label>Email Address</label>
+          <label> Your Email Address</label>
           <input type="text" name="email" placeholder="Enter your email" required>
         </div>
         <div class="field input">
